c30271c1e2db8a75cf04987294a8e333798492c7
W-ptr <wisnu.putra001@binus.ac.id>
2024-11-25 17:40:48 +0700

    feat: add seeder

--- database/migrations/2024_11_24_162255_create_events_table.php
+++ database/migrations/2024_11_24_162255_create_events_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string("title");
             $table->string("description");
             $table->date("date");
+            $table->integer("donationTotal");
             $table->string("image");
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Donation;
+use App\Models\Event;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,70 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'user 1',
+            'email' => 'test1@example.com',
+            'type' => 'donator',
+            'password' => Hash::make('123')
+        ]);
+
+        User::create([
+            'name' => 'user 2',
+            'email' => 'test2@example.com',
+            'type' => 'donator',
+            'password' => Hash::make('123')
+        ]);
+
+        User::create([
+            'name' => 'user 3',
+            'email' => 'test3@example.com',
+            'type' => 'donator',
+            'password' => Hash::make('123')
+        ]);
+
+        User::create([
+            'name' => 'admin 1',
+            'email' => 'admin@example.com',
+            'type' => 'admin',
+            'password' => Hash::make('123')
+        ]);
+
+        Event::create([
+            'title' => 'event 1',
+            'description' => 'description 1',
+            'date' => (new \DateTime('2025-11-24 10:00:00'))->format('Y-m-d H:i:s'),
+            'image' => asset("images/example.jpeg"),
+            'donationTotal' => 10000
+        ]);
+
+        Event::create([
+            'title' => 'event 2',
+            'description' => 'description 2',
+            'date' => (new \DateTime('2025-11-24 10:00:00'))->format('Y-m-d H:i:s'),
+            'image' => asset("images/example.jpeg"),
+            'donationTotal' => 20000
+        ]);
+
+        Event::create([
+            'title' => 'event 3',
+            'description' => 'description 3',
+            'date' => (new \DateTime('2025-11-24 10:00:00'))->format('Y-m-d H:i:s'),
+            'image' => asset("images/example.jpeg"),
+            'donationTotal' => 50000
+        ]);
+
+        Donation::create([
+            'event_id' => 1,
+            'user_id' => 1,
+            'amount' => 10000,
+            'date' => now()
+        ]);
+
+        Donation::create([
+            'event_id' => 2,
+            'user_id' => 1,
+            'amount' => 20000,
+            'date' => now()
         ]);
     }
 }
